962c67081ddc5a7e05fe6353ee815f54efac9a81
Anilcan <Anilcan@LAPTOP-K3V215HA>
2021-05-01 15:18:12 +0300

    Created Category class

---
+++ BlogProject.Entities/Concrete/Category.cs
@@ -0,0 +1,12 @@
+﻿using BlogProject.Shared.Entities.Abstract;
+using System.Collections.Generic;
+
+namespace BlogProject.Entities.Concrete
+{
+    public class Category : EntityBase, IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        //public ICollection<Article> Articles { get; set; }
+    }
+}
