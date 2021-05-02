b82f707482422a69c5c6cd18fd34a41c0b18b2c3
Anilcan <Anilcan@LAPTOP-K3V215HA>
2021-05-02 13:42:20 +0300

    Created DbContext and some updates

---
+++ BlogProject.Data/Concrete/EntityFramework/Contexts/BlogProjectContext.cs
@@ -0,0 +1,24 @@
+﻿using BlogProject.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Data.Concrete.EntityFramework.Contexts
+{
+    public class BlogProjectContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=BlogProjectDb;User Id=sa;Password=123;MultipleActiveResultSets=true;Connect Timeout=30;");
+        }
+    }
+}
