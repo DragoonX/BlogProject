7fbd4f003018826a113f9bd3d4901271838e25ad
Anilcan <Anilcan@LAPTOP-K3V215HA>
2021-05-02 13:18:16 +0300

    Created Repository from derivated classes

---
+++ BlogProject.Data/Concrete/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using BlogProject.Data.Abstract;
+using BlogProject.Entities.Concrete;
+using BlogProject.Shared.Data.Concrete.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogProject.Data.Concrete
+{
+    public class UserRepository : EfEntityRepositoryBase<User>, IUserRepository
+    {
+        public UserRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
