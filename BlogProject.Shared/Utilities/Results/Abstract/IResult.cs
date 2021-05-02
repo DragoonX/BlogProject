13cf79dbcb2ea713c4c07729006abaf3e27b2d2e
Anilcan <Anilcan@LAPTOP-K3V215HA>
2021-05-02 23:33:14 +0300

    Created Result return type

---
+++ BlogProject.Shared/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,12 @@
+﻿using BlogProject.Shared.Utilities.Results.ComplexTypes;
+using System;
+
+namespace BlogProject.Shared.Utilities.Results.Abstract
+{
+    public interface IResult
+    {
+        public ResultStatus ResultStatus { get; } //kullanımı: ResultStatus.Success veya ResultSuccess.Error
+        public string Message { get; }
+        public Exception Exception { get; }
+    }
+}
---
+++ BlogProject.Shared/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,34 @@
+﻿using BlogProject.Shared.Utilities.Results.Abstract;
+using BlogProject.Shared.Utilities.Results.ComplexTypes;
+using System;
+
+namespace BlogProject.Shared.Utilities.Results.Concrete
+{
+    public class Result : IResult
+    {
+        public Result(ResultStatus resultStatus)
+        {
+            ResultStatus = resultStatus;
+        }
+
+        public Result(ResultStatus resultStatus, string message)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+        }
+        public Result(ResultStatus resultStatus, string message, Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Exception = exception;
+        }
+
+        public ResultStatus ResultStatus { get; }
+
+        public string Message { get; }
+
+        public Exception Exception { get; }
+
+        //kullanımı: Result(ResultStatus.Error, exception.message, exception)
+    }
+}
