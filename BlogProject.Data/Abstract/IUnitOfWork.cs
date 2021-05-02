9b49b0521bdd077bc2a4a48ece573c3c2baa9eb3
Anilcan <Anilcan@LAPTOP-K3V215HA>
2021-05-02 17:13:15 +0300

    Created Unit Of Work pattern

---
+++ BlogProject.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BlogProject.Data.Abstract
+{
+    public interface IUnitOfWork : IAsyncDisposable //IAsyncDisposable, C# 8.0 sürümü ile ortaya çıkmıştır. İmplemente sırasında DisposeAsync() olarak çıkacaktır.
+    {
+        IArticleRepository Articles { get; } // Orn kullanım: _unitOfWork.Articles.AddAsync()
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; }
+        // _unitOfWork.Articles.AddAsync(article);
+        // _unitOfWork.Comments.AddAsync(comment);
+        // _unitOfWork.SaveAsync();
+        Task<int> SaveAsync();
+    }
+}
---
+++ BlogProject.Data/Concrete/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using BlogProject.Data.Abstract;
+using BlogProject.Data.Concrete.EntityFramework.Contexts;
+using BlogProject.Data.Concrete.EntityFramework.Repositories;
+using System.Threading.Tasks;
+
+namespace BlogProject.Data.Concrete
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly BlogProjectContext _context;
+        private EfArticleRepository _articleRepository; //new leme yapılacağı için readonly eklemiyoruz.
+        private EfCategoryRepository _categoryRepository;
+        private EfCommentRepository _commentRepository;
+        private EfRoleRepository _roleRepository;
+        private EfUserRepository _userRepository;
+
+        public UnitOfWork(BlogProjectContext context)
+        {
+            _context = context;
+        }
+
+        public IArticleRepository Articles => _articleRepository ?? new EfArticleRepository(_context);
+
+        public ICategoryRepository Categories => _categoryRepository ?? new EfCategoryRepository(_context);
+
+        public ICommentRepository Comments => _commentRepository ?? new EfCommentRepository(_context);
+
+        public IRoleRepository Roles => _roleRepository ?? new EfRoleRepository(_context);
+
+        public IUserRepository Users => _userRepository ?? new EfUserRepository(_context);
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
