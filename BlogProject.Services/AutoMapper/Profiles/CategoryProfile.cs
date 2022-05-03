67e50c74b9f2e2b030b40977986f35052ad5aa8c
DragoonX <subzero.16@hotmail.com>
2022-05-04 01:16:06 +0300

    Installed AutoMapper and Created map profiles

---
+++ BlogProject.Services/AutoMapper/Profiles/CategoryProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BlogProject.Entities.Concrete;
+using BlogProject.Entities.Dtos;
+using System;
+
+namespace BlogProject.Services.AutoMapper.Profiles
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<CategoryAddDto, Category>()
+                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(x => DateTime.Now));
+            CreateMap<CategoryUpdateDto, Category>()
+                .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(x => DateTime.Now));
+        }
+    }
+}
