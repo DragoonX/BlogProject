841ba341c369b187dc4e05c22ee67eb604c4966c
DragoonX <subzero.16@hotmail.com>
2022-09-03 23:58:27 +0300

    added UserController, User index view and UserListDto

---
+++ BlogProject.Entities/Dtos/UserListDto.cs
@@ -0,0 +1,11 @@
+﻿using BlogProject.Entities.Concrete;
+using BlogProject.Shared.Entities.Abstract;
+using System.Collections.Generic;
+
+namespace BlogProject.Entities.Dtos
+{
+    public class UserListDto : DtoGetBase
+    {
+        public IList<User> Users { get; set; }
+    }
+}
---
+++ BlogProject.Mvc/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using BlogProject.Entities.Concrete;
+using BlogProject.Entities.Dtos;
+using BlogProject.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace BlogProject.Mvc.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Area("Admin")]
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return View(new UserListDto
+            {
+                Users = users,
+                ResultStatus = ResultStatus.Success
+            });
+        }
+    }
+}
