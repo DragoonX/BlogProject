ade4047cc8265e125867f2dcb6a90fc367885f15
DragoonX <subzero.16@hotmail.com>
2022-09-04 05:22:18 +0300

    updated add function on usercontroller

---
+++ BlogProject.Entities/Dtos/UserDto.cs
@@ -0,0 +1,10 @@
+﻿using BlogProject.Entities.Concrete;
+using BlogProject.Shared.Entities.Abstract;
+
+namespace BlogProject.Entities.Dtos
+{
+    public class UserDto:DtoGetBase
+    {
+        public User User { get; set; }
+    }
+}
