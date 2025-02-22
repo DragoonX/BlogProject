fb14aefc03b26d383b7575ff9f8bcfae39568072
Anılcan Şaşkın <anilcansaskin@hotmail.com>
2025-02-22 03:35:33 +0300

    topbar updated as viewcomponent

---
+++ BlogProject.Mvc/Areas/Admin/ViewComponents/UserMenuViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using BlogProject.Entities.Concrete;
+using BlogProject.Mvc.Areas.Admin.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.Mvc.Areas.Admin.ViewComponents
+{
+    public class UserMenuViewComponent : ViewComponent
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserMenuViewComponent(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var user = _userManager.GetUserAsync(HttpContext.User).Result;
+
+            return View(new UserViewModel
+            {
+                User = user
+            });
+        }
+    }
+}
